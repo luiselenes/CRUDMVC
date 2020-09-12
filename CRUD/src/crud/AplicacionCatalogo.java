a3f23337ea9e16d9996339b6b7d8fab4d191d9ac
tania <taniabastidas32@gmail.com>
2020-09-12 15:36:44 -0600

    Vista Aplicacion

---
+++ CRUD/src/crud/AplicacionCatalogo.java
@@ -0,0 +1,19 @@
+
+package crud;
+
+/**
+ *
+ * @author Tania Nafarrate B
+ */
+public class AplicacionCatalogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       VistaCatalogo Vista=new VistaCatalogo();
+			Vista.Muestrate();
+
+    }
+    
+}

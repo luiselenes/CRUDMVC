ca30e152fcfaa8dfe39b95340884c2bac0f17d0c
tania <taniabastidas32@gmail.com>
2020-09-12 15:43:24 -0600

    Vista

---
+++ CRUD/src/crud/VistaCatalogo.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+/**
+ *
+ * @author Tania Nafarrate B
+ */
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class VistaCatalogo extends JFrame{
+    
+    JTextField RFC;
+	JTextField Nombre;
+	JTextField Edad;
+	JTextField IDCiudad;
+	
+	JLabel ERFC;
+	JLabel ENombre;
+	JLabel EEdad;
+	JLabel EIDCiudad;
+	
+	JButton BtnGrabar;
+	JButton BtnBorrar;
+	JButton BtnRecuperar;
+	JButton BtnModificar;
+	JButton BtnConsultar;
+	
+	public VistaCatalogo() {
+		super("Catalogo de Clientes");
+		HazInterfaz();
+	}
+	private void HazInterfaz() {
+		setSize(250,150);
+		setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout(0,2));
+		RFC=new JTextField();
+		Nombre=new JTextField();
+		Edad=new JTextField();
+		IDCiudad=new JTextField();
+		
+		BtnGrabar=new JButton("Grabar");
+		BtnBorrar=new JButton("Borrar");
+		BtnRecuperar = new JButton("Recuperar");
+		BtnModificar = new JButton("Modificar");
+		BtnConsultar = new JButton("Consultar");
+		
+		add(ERFC=new JLabel("RFC: "));
+		add(RFC);
+		
+		add(ENombre=new JLabel("Nombre: "));
+		add(Nombre);
+		
+		add(EEdad=new JLabel("Edad: "));
+		add(Edad);
+		
+		add(EIDCiudad=new JLabel("ID de Ciudad: "));
+		add(IDCiudad);
+		
+		add(BtnGrabar);
+		add(BtnBorrar);
+		add(BtnRecuperar);
+		add(BtnModificar);
+		add(BtnConsultar);
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+	/*public void setControlador(ConvertidorControldor c) {
+		BtnPesos.addActionListener(c);
+		BtnDolares.addActionListener(c);
+	}	*/
+	public void Muestrate() {
+		setVisible(true);
+	}
+	/*public float getCantidad() {
+		try {
+			return Float.parseFloat(Txt.getText());
+		}catch (Exception E) {
+			return 0;
+		}
+	}
+	public void setResultado(float Cantidad) {
+		Resultado.setText(Cantidad+"");
+	}*/
+    
+}
